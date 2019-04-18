0834d27cbd42736b972a10953c6a944080e51798
yidahc <yidah.a.curiel@gmail.com>
2019-04-18 15:14:15 -0500

    fixed date so it automatically receives the current timestamp when inserting

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ USE calories;
 CREATE TABLE userInfo (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
-  date DATETIME DEFAULT CURRENT_TIMESTAMP,
+  date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
   weight INT NOT NULL,
   height INT NOT NULL,
   gender ENUM("male", "female"),
@@ -22,8 +22,8 @@ CREATE TABLE userInfo (
  *  to create the database and the tables.*/
 
 
- INSERT INTO userInfo (id, name, date, weight, height, gender, BMI, calories_needed, calories_eaten) 
-               VALUES (null, "Carlos", null, 75, 60, "male", 20, 2500, 2000);
+ INSERT INTO userInfo (id, name, weight, height, gender, BMI, calories_needed, calories_eaten) 
+               VALUES (null, "Carlos", 75, 60, "male", 20, 2500, 2000);
 
 
  /*
