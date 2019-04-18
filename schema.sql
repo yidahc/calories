07b7db8c6b8dd58d2150695da267b3ac1c5bc564
yidahc <yidah.a.curiel@gmail.com>
2019-04-18 15:06:02 -0500

    fixed schemato take integers for date required to calculate BMI

--- schema.sql
+++ schema.sql
@@ -8,12 +8,12 @@ CREATE TABLE userInfo (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   date DATETIME DEFAULT CURRENT_TIMESTAMP,
-  weight SMALLINT NOT NULL,
-  height VARCHAR(20) NOT NULL,
-  gender ENUM("male", "female")
-  BMI SMALLINT NOT NULL,
-  calories_needed SMALLINT NOT NULL,
-  calories_eaten SMALLINT 
+  weight INT NOT NULL,
+  height INT NOT NULL,
+  gender ENUM("male", "female"),
+  BMI INT NOT NULL,
+  calories_needed INT NOT NULL,
+  calories_eaten INT NOT NULL
 );
 
 
@@ -22,4 +22,5 @@ CREATE TABLE userInfo (
  *  to create the database and the tables.*/
 
 
- INSERT INTO userInfo (name, weight, height, gender, BMI, calories_needed, calories_eaten) VALUES ("Carlos", 75, "5ft / 6inches","male", 2,500, 2,000);
+ INSERT INTO userInfo (id, name, date, weight, height, gender, BMI, calories_needed, calories_eaten) 
+               VALUES (null, "Carlos", null, 75, 60, "male", 20, 2500, 2000);
