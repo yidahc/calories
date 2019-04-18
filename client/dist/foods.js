e9601cde18a51650fb9a5e7dc314844262f2cc42
yidahc <yidah.a.curiel@gmail.com>
2019-04-18 02:22:03 -0500

    getting response from api to console (ran test search)

--- client/dist/foods.js
+++ client/dist/foods.js
@@ -1,6 +1,12 @@
 const apiURL = "https://api.edamam.com/api/food-database/parser?ingr=";
 const apiKey = "&app_id=6faf9cc7&app_key=5c9b29569582527e3c87130f130cb58d";
 
+function findAndReplace(string, target, replacement) {
+  for (var i = 0; i < string.length; i++) {
+    s = string.replace(target, replacement);
+  }
+  return string;
+}
 const fetchCals = ingredient => {
   var ing = findAndReplace(ingredient, " ", "%20");
     const url =  `${apiURL}${ing}${apiKey}`;
@@ -12,9 +18,6 @@ const fetchCals = ingredient => {
     });
 };
 
-function findAndReplace(string, target, replacement) {
-  for (var i = 0; i < string.length; i++) {
-    s = s.replace(target, replacement);
-  }
-  return string;
-}
+
+
+fetchCals("red apple")
