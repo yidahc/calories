9e87bcbead2278defb30bbac1ba04bd188ab2ccd
yidahc <yidah.a.curiel@gmail.com>
2019-04-18 13:00:42 -0500

    added sample data

--- schema.sql
+++ schema.sql
@@ -8,12 +8,12 @@ CREATE TABLE userInfo (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   date DATETIME DEFAULT CURRENT_TIMESTAMP,
-  weight VARCHAR(20) NOT NULL
-  height VARCHAR(20) NOT NULL
+  weight SMALLINT NOT NULL,
+  height VARCHAR(20) NOT NULL,
   gender ENUM("male", "female")
-  BMI INT NOT NULL,
-  calories_needed INT NOT NULL,
-  calories_eaten INT 
+  BMI SMALLINT NOT NULL,
+  calories_needed SMALLINT NOT NULL,
+  calories_eaten SMALLINT 
 );
 
 
@@ -22,4 +22,4 @@ CREATE TABLE userInfo (
  *  to create the database and the tables.*/
 
 
- INSERT INTO userInfo (name, weight, height, gender, BMI, calories) VALUES ("Carlos", );
+ INSERT INTO userInfo (name, weight, height, gender, BMI, calories_needed, calories_eaten) VALUES ("Carlos", 75, "5ft / 6inches","male", 2,500, 2,000);
