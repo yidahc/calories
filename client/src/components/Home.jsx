675ecef3c643ebee6fbf0954d9a1bd5e5a937ab5
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-04-19 20:00:39 -0500

    still working on rendering css

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
-import "./home.css"
+
 
 
 class Home extends Component {
 	render(){
 		return (
 			<div>
-			  <center><h1> &lt; Calories </h1></center>
+			  <center><h1 className ="Header"> &lt; Calories </h1></center>
 			  <center><button><Link to="/BMI">Get Started</Link></button></center>
 			</div>
 		)
